d0908a50ac7a152285a405429562bf50df9ee384
zach wegrzyniak <zwegrzyniak@gmail.com>
2014-01-20 16:24:19 -0700

    database up

--- Parser.py
+++ Parser.py
@@ -9,7 +9,7 @@ from unigram import Unigram, db
 def tokenize(string):
 	string = string.lower()
 	for unigram in string.split():
-		stripped = unigram.lstrip('>,(?*[\'(" ').rstrip(':;.,?!)"-] ')
+		stripped = unigram.lstrip('>,(?*[\'(" ').rstrip(':;.,?!)\"-] ')
 		if stripped[-2:] != 's\'':
 			stripped = stripped.rstrip("'")
 		
@@ -42,10 +42,29 @@ def get_comment_data_blobs(directory='data'):
 				for line in prepped_zip_file.open(raw_file, 'r'):
 					yield json.loads(line.decode())
 
+
+def dump_into_db(db, counts):
+	print('Moving from memory into database.')
+	for word in counts:
+		current = Unigram.query.filter(Unigram.id==word).first()
+		if current:
+			current.times_occurred += counts[word]
+		else:
+			current = Unigram(word, counts[word])
+		db.session.add(current)
+		db.session.commit()
+
+	counts = {}
+	print('Leaving dump.')
+
+
 db.create_all()
 word_counts = {}
 try:
 	for blob in get_comment_data_blobs():
+		if len(word_counts) > 100000:
+			dump_into_db(db, word_counts)
+			raise KeyboardInterrupt
 		body = blob['body']
 		for unigram in tokenize(body):
 			if unigram in word_counts:
@@ -53,18 +72,12 @@ try:
 			else:
 				word_counts[unigram] = 1
 
-except (Exception, KeyboardInterrupt) as e:
+except (KeyboardInterrupt) as e:
 	print(e)
+	dump_into_db(db, word_counts)
 
 finally:
-	final_count = open('count.txt', 'w')
-	without_ones = open('without_ones.txt', 'w')
-
-	unsorted_list = Counter(word_counts)
-	raw_sorted = unsorted_list.most_common()
-	for item in raw_sorted:
-		current_item = '{} -> {}\n'.format(*item)
-		final_count.write(current_item)
-		if item[1] > 1:
-			without_ones.write(current_item)
+	final_count = sorted(Unigram.query.all(), reverse=True)
+	for unigram in final_count:
+		print('{}'.format(unigram))
 
