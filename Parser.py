c11885d87094efd8225ad9e1877f941b6a771422
zach wegrzyniak <zwegrzyniak@gmail.com>
2014-01-30 01:13:50 -0700

    added animal suffering back in

--- Parser.py
+++ Parser.py
@@ -1,84 +1,134 @@
 import os
 import json
-from collections import Counter
+from copy import deepcopy
+from collections import deque
+from datetime import datetime
 from zipfile import BadZipFile, ZipFile
 
-from unigram import Unigram, db
+from bookmark import Bookmark
+from unigram import Unigram, Comment, db
+
+class Parser:
+
+
+    def __init__(self):
+        try:
+            stopped_at = open('.bookmark')
+            stopping_place = eval(stopped_at.readline())
+            self.current_position = Bookmark(*stopping_place)
+        except FileNotFoundError:
+            self.current_position = Bookmark()
+
+        self.word_counts = {}
+        self.place_saved = Bookmark()
+        db.create_all()
+
+    def save_place(self):
+        with open('.bookmark', 'w') as stopped_at:
+            stopped_at.write('{}'.format(self.place_saved))
+
+    def get_comment_data_blobs(self, directory='data'):
+        for zipped_file in os.walk(directory):
+            for file_name in zipped_file[2]:
+                if file_name[-17:] != 'comments.json.zip' \
+                    or not self.current_position.right_file(file_name):
+                    print('Skipped parsing {}'.format(file_name))
+                    continue
+                try:
+                    prepped_zip_file = ZipFile('data/' + file_name)
+                    print(file_name)
+                except BadZipFile as e:
+                    continue
+
+                for raw_file in prepped_zip_file.namelist():
+                    for line in prepped_zip_file.open(raw_file, 'r'):
+                        yield json.loads(line.decode()), file_name
+
+    def run(self):
+        try:
+            for blob, current_file in self.get_comment_data_blobs():
+                comment = build_comment(blob)
+                if not self.current_position.back_where_we_need_to_be(current_file, comment.id):
+                    continue
+
+                db.session.add(comment)
+
+                if len(self.word_counts) > 100000:
+                    self.dump_into_db(db)
+
+                for unigram in tokenize(blob['body']):
+                    if unigram in self.word_counts:
+                        self.word_counts[unigram]['count'] += 1
+                    else:
+                        self.word_counts[unigram] = {}
+                        self.word_counts[unigram]['count'] = 1
+
+                    if not self.word_counts[unigram].get('occurences'):
+                        self.word_counts[unigram]['occurences'] = deque()
+
+                    self.word_counts[unigram]['occurences'].append(comment)
+                self.current_position.commit = True
+
+        except (KeyboardInterrupt) as e:
+            print(e)
+            self.dump_into_db(db)
+
+        finally:
+            final_count = sorted(Unigram.query.all(), reverse=True)
+            for unigram in final_count:
+                print('{}'.format(unigram))
+
+    def dump_into_db(self, db):
+        start = datetime.now()
+        length = len(self.word_counts)
+        alert = '\nMoving from memory into database. ({})'
+        db.session.commit()
+
+        print(alert.format(length))
+
+        for current in Unigram.query.filter(Unigram.id in self.word_counts).all():
+            current.times_occurred += self.word_counts[current.id]['count']
+            current.occurs_in.extend( self.word_counts[current.id]['occurences'])
+            del(self.word_counts[current.id])
+
+        for ngram in self.word_counts:
+            current = Unigram(ngram, self.word_counts[ngram]['count'],
+                                  self.word_counts[ngram]['occurences'])
+            db.session.add(current)
+
+        db.session.commit()
+        print('Leaving dump after {}.'.format(datetime.now()-start))
+        self.place_saved = deepcopy(self.current_position)
+        self.save_place()
 
 
 def tokenize(string):
-	string = string.lower()
-	for unigram in string.split():
-		stripped = unigram.lstrip('>,(?*[\'(" ').rstrip(':;.,?!)\"-] ')
-		if stripped[-2:] != 's\'':
-			stripped = stripped.rstrip("'")
-		
-		elipses = stripped.split('...')
-		dash =  stripped.split('--')
+    string = string.lower()
+    for unigram in string.split():
+        stripped = unigram.lstrip('>,(?*[\'(" ').rstrip(':;.,?!)\"-] ')
+        if stripped[-2:] != 's\'':
+            stripped = stripped.rstrip("'")
 
-		if len(elipses) > 1:
-			tokenize(' '.join(elipses))
-			continue
-		if len(dash) > 1:
-			tokenize(' '.join(dash))
-			continue
+        elipses = stripped.split('...')
+        dash = stripped.split('--')
 
-		if stripped is not None:
-			yield stripped
-		continue
-		
-def get_comment_data_blobs(directory='data'):
-	for zipped_file in os.walk(directory):
-		for file_name in zipped_file[2]: #The file component of the tuple
-			if file_name[-17:] != 'comments.json.zip':
-				continue
-			try:
-				prepped_zip_file = ZipFile('data/' + file_name)
-				print(file_name)
-			except BadZipFile as e:
-				continue
+        if len(elipses) > 1:
+            tokenize(' '.join(elipses))
+            continue
+        if len(dash) > 1:
+            tokenize(' '.join(dash))
+            continue
 
-			for raw_file in prepped_zip_file.namelist():
-				for line in prepped_zip_file.open(raw_file, 'r'):
-					yield json.loads(line.decode())
+        if stripped is not None:
+            yield stripped
+        continue
 
 
-def dump_into_db(db, counts):
-	length = len(counts)
-	alert = 'Moving from memory into database. ({})'
-	print(alert.format(length))
-	for word in counts:
-		current = Unigram.query.filter(Unigram.id==word).first()
-		if current:
-			current.times_occurred += counts[word]
-		else:
-			current = Unigram(word, counts[word])
-			db.session.add(current)
-		db.session.commit()
+def build_comment(blob):
+    comment_id = blob['_id']
+    comment_created = blob['updated_on']['$date']
+    comment_created = datetime.fromtimestamp(comment_created / 1000)
+    return Comment(comment_id, comment_created)
 
-	print('Leaving dump.')
-
-
-db.create_all()
-word_counts = {}
-try:
-	for blob in get_comment_data_blobs():
-		if len(word_counts) > 100000:
-			dump_into_db(db, word_counts)
-			word_counts = {}
-		body = blob['body']
-		for unigram in tokenize(body):
-			if unigram in word_counts:
-				word_counts[unigram] += 1
-			else:
-				word_counts[unigram] = 1
-
-except (KeyboardInterrupt) as e:
-	print(e)
-	dump_into_db(db, word_counts)
-
-finally:
-	final_count = sorted(Unigram.query.all(), reverse=True)
-	for unigram in final_count:
-		print('{}'.format(unigram))
-
+if __name__ == '__main__':
+    Parser().run()
---
+++ bookmark.py
@@ -0,0 +1,50 @@
+class Bookmark:
+    def __init__(self, final_file=None, final_comment=None):
+        self.last_finished_file = final_file
+        self.last_finished_comment = final_comment
+        self.at_new = False
+
+    def right_file(self, current_file):
+        if not self.last_finished_file:
+            self.at_new = True
+
+        if not self.at_new:
+            return self.last_finished_file == current_file
+        self.last_finished_file = current_file
+        return True
+
+    def right_comment(self, current_comment):
+        if not self.last_finished_comment:
+            self.at_new = True
+        if not self.at_new:
+            return self.last_finished_comment == current_comment
+        self.last_finished_comment = current_comment
+        return True
+
+    def back_where_we_need_to_be(self, current_file, current_comment):
+        self.at_new = bool(self.right_file(current_file) and self.right_comment(current_comment))
+        return self.at_new
+
+    def __repr__(self):
+            return str((self.last_finished_file, self.last_finished_comment))
+
+import unittest
+class BookMarkTest(unittest.TestCase):
+    files = 'abcde'
+    comments = range(0, 10)
+
+    def setUp(self):
+        self.b = Bookmark('c', 4)
+
+    def test_catches_correct_final_comment(self):
+        for _file in BookMarkTest.files:
+            for comment in BookMarkTest.comments:
+                if _file < self.b.last_finished_file and \
+                                comment < self.b.last_finished_comment:
+                    self.assertFalse(self.b.back_where_we_need_to_be(_file, comment))
+                elif self.b.back_where_we_need_to_be(_file, comment):
+                    self.assertEqual(self.b.last_finished_comment, comment)
+                    self.assertEqual(self.b.last_finished_file, _file)
+
+if __name__ == '__main__':
+    unittest.main()
